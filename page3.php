a40bfe82fc0694baa672995c624d1f8a8e4566e8
ninta <nintah85@gmail.com>
2023-03-07 13:53:33 +0000

    learning

---
+++ page3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+</head>
+<body>
+<h1> THE NAME </h1>
+
+<p> Hello, my name is, <?php echo "Bruno"; ?> ! </p>
+
+</body>
+</html>
